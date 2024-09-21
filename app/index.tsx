5abae1510b208a26251a6e3772860577ce47edd0
beingyash <beingyash2712@gmail.com>
2024-09-22 01:51:03 +0530

    first commit

---
+++ app/index.tsx
@@ -0,0 +1,17 @@
+import { Text, View , Image } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Image src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.facebook.com%2FLifeAtVirtusaIndia%2F&psig=AOvVaw3HwQhg9fkmgNRn21yCpORc&ust=1727036381934000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCNDYq4Lu1IgDFQAAAAAdAAAAABAE"></Image>
+      <Image></Image>
+      <Text>working</Text>
+    </View>
+  );
+}
